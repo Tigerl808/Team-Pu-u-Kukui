5c61e712fa3e3daf145a2f50b94b63002e889e72
Alii <tigerli808@live.com>
2025-07-31 19:42:08 -1000

    Update app.py

--- app.py
+++ app.py
@@ -597,8 +597,13 @@ def show_attendance_card(col):
         st.markdown("""
                 
                 <div class="card-footer">
-                    <span class="card-footer-text">  The WDD-Hi Digital Work Skills project, in partnership with the Hawaii State Public Library System (HSPLS), launched classes in 2023- 2024 with the mission to bridge the digital divide by offering free basic computer skills workshops at library branches across the state. These classes aim to equip participants with essential digital literacy skills such as using email, browsing the internet, and ensuring online safety.
-
+                    <span class="card-footer-text">The WDD-Hi Digital Work Skills project, in partnership with the Hawaii 
+State Public Library System (HSPLS), launched classes in 2023- 2024 with the mission to bridge the digital divide by offering
+free basic computer skills workshops at library branches across the state. These classes aim to equip participants with essential 
+digital literacy skills such as using email, browsing the internet, and ensuring online safety.
+                </div>
+                  <div class="card-footer">
+                    <span class="card-footer-text">In our analysis, the regression factors are: Date, Island, Branches, Classes_scheduled, Registered, Attended, Attend_Rate. 
 
                 </div>
             
